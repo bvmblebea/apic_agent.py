a08ebc3aff89e548732184e3757838760d9b2f8e
ecclesias <77536370+zeviel@users.noreply.github.com>
2022-11-19 20:30:58 +0800

    Update apic_agent.py

--- src/apic_agent.py
+++ src/apic_agent.py
@@ -1,8 +1,7 @@
 from requests import get
 
-
 class ApicAgent:
-	def __init__(self, user_agent: str):
+	def __init__(self, user_agent: str) -> None:
 		self.api = "https://api.apicagent.com"
 		self.headers = {
 			"user-agent": user_agent
@@ -12,17 +11,17 @@ class ApicAgent:
 			f"{self.api}?ua={self.user_agent}",
 			headers=self.headers).json()
 		
-	def get_user_agent_info(self):
+	def get_user_agent_info(self) -> dict:
 		return self.user_agent_info
 	
-	def get_user_agent_browser(self):
+	def get_user_agent_browser(self) -> str:
 		return self.user_agent_info["browser_family"]
 	
-	def get_user_agent_client(self):
+	def get_user_agent_client(self) -> str:
 		return self.user_agent_info["client"]
 	
-	def get_user_agent_device(self):
+	def get_user_agent_device(self) -> str:
 		return self.user_agent_info["device"]
 	
-	def get_user_agent_os(self):
+	def get_user_agent_os(self) -> str:
 		return self.user_agent_info["os"]
